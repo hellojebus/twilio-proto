943f3418455abc7cc359a957a77acb93d8ddd867
Jesus Lizama <dev@hellojeb.us>
2018-05-17 09:36:36 -0700

    lolol

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $token = getenv("TWILIO_TOKEN");
 $twilio = new Client($sid, $token);
 
 $msg = $twilio->messages->create(getenv("MY_NUMBER"), [
-  "body" => "te amo k thx bai",
+  "body" => "k thx bai",
   "from" => getenv("TWILIO_NUMBER"),
   "mediaUrl" => "https://cdn.shopify.com/s/files/1/1019/9659/products/Wong_lifeisbeautiful_webready_2048x2048.jpeg?v=1484689233"
 ]);
