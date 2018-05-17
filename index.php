32a6d226eeec80685e5d70ce21be6e925eeb48a8
Jesus Lizama <dev@hellojeb.us>
2018-05-16 21:17:20 -0700

    add inital code

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use Twilio\Rest\Client;
+
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+$sid = getenv("TWILIO_SID");
+$token = getenv("TWILIO_TOKEN");
+$twilio = new Client($sid, $token);
+
+$msg = $twilio->messages->create(getenv("MY_NUMBER"), [
+  "body" => "te amo k thx bai",
+  "from" => getenv("TWILIO_NUMBER"),
+  "mediaUrl" => "https://cdn.shopify.com/s/files/1/1019/9659/products/Wong_lifeisbeautiful_webready_2048x2048.jpeg?v=1484689233"
+]);
+
+echo json_encode($msg);
+
+?>
